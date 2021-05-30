aef0ec498e6fdd3973ba8285a8e6323fc89879da
code-by-mandy <mandycodes2@gmail.com>
2021-05-30 19:57:55 -0400

    filter - category and search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import firebase from './firebase'
-import {useEffect, useState} from 'react'
+import {useEffect, useState, useRef} from 'react'
 
 function App() {
 
   const [solutions, setSolutions] = useState([]);
   const [filtered, setFiltered] = useState([]);
+  const [filterCategory, setFilterCategory] = useState("all");
+  const [searchString, setSearchString] = useState("");
+  const searchInput = useRef(null);
+  const selectInput = useRef(null);
 
   useEffect( () => {
     const dbRef = firebase.database().ref();
@@ -15,17 +19,30 @@ function App() {
       for (let solution in data) {
         newState.push(data[solution]);
       }
-
       setSolutions(newState);
     });
   }, []);
-  
-  const handleSearch = (e) =>{
 
-    let regex = new RegExp(e.target.value.toLowerCase());
+  const handleSearch = (e) => {
+    e.preventDefault();
 
-    const filteredArray = solutions.filter(solution => solution.solution.toLowerCase().match(regex) || solution.category.match(regex));
-    setFiltered(filteredArray);
+    let searchRegex = new RegExp(searchString);
+    let filterRegex = new RegExp(filterCategory);
+
+    if (searchString === "" && filterCategory === "all") {
+      const filteredArray = solutions;
+      setFiltered(filteredArray);
+    } else if (searchString === "" && filterCategory) {
+      const filteredArray = solutions.filter(solution => solution.category.match(filterRegex));
+      setFiltered(filteredArray);
+    } else if (searchString && filterCategory === "all") {
+      const filteredArray = solutions.filter( solution => solution.solution.toLowerCase().match(searchRegex));
+      setFiltered(filteredArray);
+    } else {
+      const filteredArray = solutions.filter( solution => solution.solution.toLowerCase().match(searchRegex) && solution.category.match(filterRegex));
+      setFiltered(filteredArray);
+    }
+
   }
 
   return (
@@ -33,31 +50,45 @@ function App() {
       <div>
         <h1>Filter solutions identified by the Drawdown Project!</h1>
         <p>All data from the Drawdown Project.</p>
-        <form>
+        <form onSubmit={handleSearch}>
+          <div>
             <label htmlFor="search">Search:</label>
-            <input id="search" type="search" onKeyUp={handleSearch}></input>
-        </form>
-        <form>
-          <label htmlFor="categories">Filter by category</label>
-          <select id="categories">
-            <option defaultValue>Choose one:</option>
-            <option>Buildings</option>
-            <option>Cities</option>
-            <option>Energy</option>
-            <option>Food</option>
-            <option>Land</option>
-            <option>Materials</option>
-            <option>Transport</option>
-            <option>Women</option>
+            <input id="search" type="search" onKeyUp={(e) => setSearchString(e.target.value.toLowerCase())} ref={searchInput}></input>
+          </div>
+          <div>
+            <label htmlFor="categories">Filter by category</label>
+            <select id="categories" onChange={(e) => setFilterCategory(e.target.value)} ref={selectInput}>
+              <option value = "all" defaultValue>Choose one:</option>
+              <option value="buildings">Buildings</option>
+              <option value="cities">Cities</option>
+              <option value="energy">Energy</option>
+              <option value="food">Food</option>
+              <option value="land">Land</option>
+              <option value="materials">Materials</option>
+              <option value="transport">Transport</option>
+            <option value="women">Women</option>
           </select>
+          </div>
+          <button type="submit">Go</button>
         </form>
-        <ul>
+        <div>
+                { 
+                  searchString === "" && filterCategory === "all" ? <h2>All Solutions:</h2>  
+                  : searchString === "" ? <h2>Solutions in the category "{filterCategory}"</h2>
+                  : filterCategory === "all" ? <h2>Search results for "{searchString}"</h2>
+                  : <h2>Search results for "{searchString}" in category "{filterCategory}"</h2>
+                }
+                
+          <ul>
           { filtered.map( solution => {
             return(
-              <li key={solutions.indexOf(solution)}><a href={solution.url}>{solution.solution}</a></li>
+              <li key={filtered.indexOf(solution)}><a href={solution.url}>{solution.solution}</a></li>
             )
           })}
-        </ul>
+          </ul>        
+        </div>
+        
+        
 
       </div>      
     </div>
